19134dd58ac33eb677c2adeec23ecc8b3df2d54c
Cliff Wulfman <cwulfman@princeton.edu>
2022-01-20 18:18:54 -0500

    first commit of adam code

---
+++ adam/src/adam/page.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# page.py
+"""The Page module encapsulates OCR and NER.  Eventually it could be
+re-factored to take OCR and NER objects as injections."""
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+class Page:
+    """Page class"""
+    def __init__(self, page_image):
+        self._image_file = page_image
+        self._text = False
+        self._hocr = False
+        self._alto = False
+
+    @property
+    def image_file(self):
+        return self._image_file
+
+    @property
+    def text(self):
+        if not self._text:
+            self.do_ocr_to_string()
+        return self._text
+
+    @property
+    def hocr(self):
+        if not self._hocr:
+            self.do_ocr_to_hocr()
+        return self._hocr
+
+    @property
+    def alto(self):
+        if not self._alto:
+            self.do_ocr_to_alto()
+        return self._alto
+
+    def do_ocr_to_string(self):
+        self._text = pytesseract.image_to_string(Image.open(self.image_file))
+
+    def do_ocr_to_hocr(self):
+        self._hocr = pytesseract.image_to_pdf_or_hocr(
+            Image.open(self.image_file), extension='hocr')
+
+    def do_ocr_to_alto(self):
+        self._alto = pytesseract.image_to_alto_xml(
+            Image.open(self.image_file))
---
+++ adam/tests/test_page.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+from adam.page import Page
+
+testable_image = os.path.join(
+    os.path.dirname(__file__),
+    "testable_image.tif")
+
+
+@pytest.fixture(name="basic_page")
+def fixture_basic_page():
+    """Returns empty page"""
+    return Page(testable_image)
+
+
+def test_simple(basic_page):
+    assert basic_page.image_file == testable_image
+
+
+def test_text(basic_page):
+    assert len(basic_page.text) > 0
+
+def test_hocr(basic_page):
+    assert len(basic_page.hocr) > 0
+
+def test_alto(basic_page):
+    assert len(basic_page.alto) > 0
