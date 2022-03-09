7c017ed3f5bbfef3c2b7799c9d8498ae558535ad
Cliff Wulfman <cwulfman@princeton.edu>
2022-03-09 16:15:43 -0500

    bug fixes

--- adam/src/adam/global_vars.py
+++ adam/src/adam/global_vars.py
@@ -2,3 +2,4 @@
 from pathlib import Path
 
 PAGE_IMAGE_CACHE = Path("/tmp/page_images")
+PAGE_IMAGE_CACHE.mkdir(parents=True, exist_ok=True)
--- adam/src/adam/page.py
+++ adam/src/adam/page.py
@@ -18,7 +18,6 @@ import os
 import logging
 import requests
 # import urllib.request
-from cv2 import imread
 from PIL import Image
 import pytesseract
 import pandas
