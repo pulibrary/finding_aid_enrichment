3fa6990b17a4cd70b2a217e372e2e3b7f39d80f8
cwulfman <cwulfman@princeton.edu>
2022-09-14 19:26:32 -0400

    updates appellation_builder

--- adam/src/adam/appellation_builder.py
+++ adam/src/adam/appellation_builder.py
@@ -17,26 +17,41 @@ canvas
 inscription
 """
 
+import re
 import sys
 from pathlib import Path
 import csv
 import argparse
 from sys import stdout
 import logging
+from adam.appellation import Appellation
 
 
-path = "/Users/cwulfman/Downloads/freq-20-15-csv.tsv"
+path = "/Users/cwulfman/Desktop/ColdWarPapers/MC076/openrefine_data_A-D.csv"
+
 
 appellations = {}
 
 with open(path, 'r') as data_file:
-    reader = csv.DictReader(data_file, delimiter='\t')
-    for row in reader:
+    reader = csv.DictReader(data_file)
+    records = list(reader)
+    
+for row in records:
+    if 'appellation' in row:
         appellation = row['appellation']
         if appellation not in appellations.keys():
             appellations[appellation] = {}
             appellations[appellation]['viaf_id'] = row['VIAF_ID']
             appellations[appellation]['inscriptions'] = []
-
         appellations[appellation]['inscriptions'].append(row['inscription'])
-        
+            
+objects = []
+for k in appellations.keys():
+    viaf_id = appellations[k]['viaf_id']
+    inscriptions = appellations[k]['inscriptions']
+    objects.append(Appellation(k, viaf_id, inscriptions))
+
+for o in objects:
+    o.build_graph()
+    fname = re.sub('[. ]', '', o.label)
+    o.serialize('/tmp/output/' + fname + '.ttl')
